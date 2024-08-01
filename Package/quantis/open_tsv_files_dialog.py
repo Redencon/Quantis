bfb258a7f4487740441dc5c6204bd9075182de3c
Daniil Pomogaev <55309680+Redencon@users.noreply.github.com>
2024-08-01 18:16:31 +0300

    Add Scavager+Diffacto workflow

--- Package/quantis/open_tsv_files_dialog.py
+++ Package/quantis/open_tsv_files_dialog.py
@@ -8,6 +8,14 @@ def open_tsv_files_dialog(window: webview.Window, multiple: bool = False):
     )
     return result
 
+def open_exe_files_dialog(window: webview.Window, multiple: bool = False):
+    file_types = ('Executables (*.*)',)
+
+    result = window.create_file_dialog(
+        webview.OPEN_DIALOG, allow_multiple=multiple, file_types=file_types
+    )
+    return result
+
 def save_csv_file_dialog(window: webview.Window):
     file_types = ('CSV Files (*.csv)',)
 
