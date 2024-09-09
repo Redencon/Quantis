c799147fa70d082a8981d5265b40f24f84acd195
Daniil Pomogaev <55309680+Redencon@users.noreply.github.com>
2024-09-09 21:59:48 +0300

    Temporarily removed dash-uploader

--- Package/quantis/app.py
+++ Package/quantis/app.py
@@ -1,7 +1,7 @@
 # from . import log_setup
 from dash import Dash, dcc, callback, Input, Output, State, html, no_update, dash_table, ctx
 import dash_bootstrap_components as dbc
-import dash_uploader as du
+# import dash_uploader as du
 from dash_daq.ColorPicker import ColorPicker
 from pathlib import Path
 import pandas as pd
@@ -43,7 +43,7 @@ def resource_path(relative_path):
 
 # app = Dash("Quantis", external_stylesheets=["style.css"])
 app = Dash("Quantis", title="Quantis", assets_folder=str(resource_path("assets")))
-du.configure_upload(app, FILES_PATH, use_upload_id=False)
+# du.configure_upload(app, FILES_PATH, use_upload_id=False)
 
 
 app.layout = html.Div([
@@ -139,18 +139,18 @@ app.layout = html.Div([
                     ], style={'display': 'flex', 'flex-direction': 'row', 'justify-content': 'space-between'}),
                 ], style={"padding": 10, 'flex': 1}),
             ], style={'display': 'flex', 'flex-direction': 'row'}),
-            dcc.Download(id="download_sample"),
-            html.Button("Generate Sample File", id="sample_gen_btn", className="download_button"),
-            html.P("Or use a sample file:"),
-            du.Upload(id="sample_input", filetypes=['csv'], text="Upload Sample File", max_files=1),
-            html.Details([
-                html.Summary("What is sample file?", style={'font-size': '12pt'}),
-                html.P(
-                    "Sample file is a csv file, containing informtion about input files. "
-                    + "It is expected to have two columns: Path with absolute paths to files "
-                    + "and Sample, containing letters K and A for control and test files respectfully."
-                )
-            ]),
+            # dcc.Download(id="download_sample"),
+            # html.Button("Generate Sample File", id="sample_gen_btn", className="download_button"),
+            # html.P("Or use a sample file:"),
+            # du.Upload(id="sample_input", filetypes=['csv'], text="Upload Sample File", max_files=1),
+            # html.Details([
+            #     html.Summary("What is sample file?", style={'font-size': '12pt'}),
+            #     html.P(
+            #         "Sample file is a csv file, containing informtion about input files. "
+            #         + "It is expected to have two columns: Path with absolute paths to files "
+            #         + "and Sample, containing letters K and A for control and test files respectfully."
+            #     )
+            # ]),
         ], style={"diplay": "grid"}, id="multi_input_div"),
         html.Br(),
 
@@ -611,20 +611,20 @@ def generate_sample_file(_, k_data, a_data):
 
 
 # Select files with sample file
-@du.callback(
-    [Output("control_files_table", "data", allow_duplicate=True),
-    Output("test_files_table", "data", allow_duplicate=True)],
-    id="sample_input"
-)
-def load_sample_file(status: du.UploadStatus):
-    if not status.uploaded_files:
-        return no_update, no_update
+# @du.callback(
+#     [Output("control_files_table", "data", allow_duplicate=True),
+#     Output("test_files_table", "data", allow_duplicate=True)],
+#     id="sample_input"
+# )
+# def load_sample_file(status: du.UploadStatus):
+#     if not status.uploaded_files:
+#         return no_update, no_update
     
-    # Load the sample file
-    sdf = pd.read_csv(FILES_PATH / status.uploaded_files[0].name)
-    k_files = [{"path": f} for f in sdf[sdf["Sample"] == "K"]["Path"].tolist()]
-    a_files = [{"path": f} for f in sdf[sdf["Sample"] == "A"]["Path"].tolist()]
-    return k_files, a_files
+#     # Load the sample file
+#     sdf = pd.read_csv(FILES_PATH / status.uploaded_files[0].name)
+#     k_files = [{"path": f} for f in sdf[sdf["Sample"] == "K"]["Path"].tolist()]
+#     a_files = [{"path": f} for f in sdf[sdf["Sample"] == "A"]["Path"].tolist()]
+#     return k_files, a_files
 
 # Enable/disable custom species input
 @callback(
